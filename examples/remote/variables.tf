2f4df7e9def7fd7c81e5dce3c35896e6fc3cebf8
Said Sef <saidsef@gmail.com>
2024-02-25 13:03:03 +0000

    chore: added variables classification

--- examples/remote/variables.tf
+++ examples/remote/variables.tf
@@ -1,9 +1,11 @@
 variable "name" {
   description = "Workspace name"
   type        = string
+  sensitive   = false
 }
 
 variable "organization" {
   description = "Terraform Cloud organization name"
   type        = string
+  sensitive   = false
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "id" {
   description = "Workspace ID"
   value       = tfe_workspace.workspace.id
+  sensitive   = false
 }
 
 output "name" {
   description = "Workspace name"
   value       = tfe_workspace.workspace.name
+  sensitive   = false
 }
