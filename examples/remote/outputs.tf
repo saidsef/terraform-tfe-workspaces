3f48b1df9885de2d03da7a8501da89dbb6e2cc9c
Said Sef <saidsef@gmail.com>
2024-02-25 12:58:40 +0000

    chore: add remote output

---
+++ examples/remote/outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  description = "Workspace ID"
+  value       = module.workspaces.id
+}
+
+output "name" {
+  description = "Workspace name"
+  value       = module.workspaces.name
+}
