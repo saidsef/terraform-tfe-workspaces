49e915e2156edcb1e7f04c727f80cb48ce02ba8c
Said Sef <saidsef@gmail.com>
2024-04-10 12:34:20 +0100

    chore: add execution mode in terraform output
    Signed-off-by: Said Sef <saidsef@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,9 @@ output "name" {
   value       = tfe_workspace.workspace.name
   sensitive   = false
 }
+
+output "execution_mode" {
+  description = "Workspace execution mode"
+  value       = tfe_workspace.workspace.execution_mode
+  sensitive   = false
+}
